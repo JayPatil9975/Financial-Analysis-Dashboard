5a96ff85512cedf50e0c40e91c2025e3d1e6da90
JayPatil9975 <jayeshpatil997594@gmail.com>
2025-06-29 16:06:55 +0530

    url

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://financial-analysis-dashboard-p8is.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
